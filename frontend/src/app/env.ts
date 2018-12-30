ec37faf9c2e0482fb9512bcdc77e3d9b38a8cef5
Ibrahim E.Gad <eibrahim95@gmail.com>
2018-12-30 12:07:23 +0200

    init prod

--- frontend/src/app/env.ts
+++ frontend/src/app/env.ts
@@ -1,2 +1,3 @@
 // export const API_URL = 'http://miltonsonodadeploy.pythonanywhere.com';
-export const API_URL = 'http://127.0.0.1:5000/api';
+// export const API_URL = 'http://127.0.0.1:5000/api';
+export const API_URL = 'http://eibrahim95.pythonanywhere.com//api';
